dc361268c399a38bf1c9956003de8bf47c2bcb43
skymish <53576743+skymish@users.noreply.github.com>
2024-08-28 14:03:34 -0400

    Update DattoRMMGenerator.psm1

--- Modules/DattoRMMGenerator.psm1
+++ Modules/DattoRMMGenerator.psm1
@@ -183,14 +183,14 @@ function Get-DRMMDeviceStatusSection {
     )
 
     # Generate CPU/ RAM Use Data
-    # $CPUData = $Device.udf."udf$CPUUDF" | convertfrom-json
-    # $RAMData = $Device.udf."udf$RAMUDF" | convertfrom-json
+    $CPUData = $Device.udf."udf$CPUUDF" | convertfrom-json
+    $RAMData = $Device.udf."udf$RAMUDF" | convertfrom-json
 
-    # $CPUUse = $CPUData.T
-    # $RAMUse = $RAMData.T
+    $CPUUse = $CPUData.T
+    $RAMUse = $RAMData.T
 
-    # $CPUTable = Get-DecodedTable -TableString $CPUData.D -UseValue '%' | convertto-html -Fragment
-    # $RAMTable = Get-DecodedTable -TableString $RAMData.D -UseValue 'GBs' | convertto-html -Fragment
+    $CPUTable = Get-DecodedTable -TableString $CPUData.D -UseValue '%' | convertto-html -Fragment
+    $RAMTable = Get-DecodedTable -TableString $RAMData.D -UseValue 'GBs' | convertto-html -Fragment
 
     $DiskData = $DeviceAudit.logicalDisks | where-object { $_.freespace }
 
